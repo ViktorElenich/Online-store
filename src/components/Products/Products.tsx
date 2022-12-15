ec6727883807b91cc29bdfa4198979652810b9cc
Viktor <velenic@mail.ru>
2022-12-15 12:37:58 +0300

    feat: add search, select components

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,17 +1,25 @@
 import ProductItem from '../ProductItem/ProductItem';
 import { productsData } from '../../data/data';
 import './Products.scss';
-import ProductsFilter from "../ProductsFilter/ProductsFilter";
+import ProductsFilter from '../ProductsFilter/ProductsFilter';
+import Search from '../Search/Search';
+import Select from "../Select/Select";
 
 const Products = () => (
   <div className='products'>
     <aside className='products-filter'>
       <ProductsFilter />
     </aside>
-    <div className='products-items'>
-      {productsData.products.map((product) => (
-        <ProductItem key={product.id} item={product} isInTheCart={false} />
-      ))}
+    <div className='products-wrapper'>
+      <div className="products-wrapper__sortSearch">
+        <Select />
+        <Search />
+      </div>
+      <div className='products-items'>
+        {productsData.products.map((product) => (
+          <ProductItem key={product.id} item={product} isInTheCart={false} />
+        ))}
+      </div>
     </div>
   </div>
 );
