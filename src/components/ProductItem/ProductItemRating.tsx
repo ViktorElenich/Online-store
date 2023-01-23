8bec53e0ccbf1ac7419094891e6e3dd84a782e50
Ekalape <ekalape@outlook.it>
2023-01-23 18:41:58 +0100

    fix: rewrite component ItemRating as a class and add noImplicitAny rule to the tsconfig

--- src/components/ProductItem/ProductItemRating.tsx
+++ src/components/ProductItem/ProductItemRating.tsx
@@ -1,19 +1,28 @@
 import './ProductItem.scss';
-
-import { FC } from 'react';
+import React /* , { FC } */ from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ratingVisualizationFn from '../../utils/ratingVisualizationFn';
 
-const ItemRating: FC<{ itemRating: number }> = (props) => {
-  const { itemRating } = props;
-  const starsArray = ratingVisualizationFn(itemRating);
+class ItemRating extends React.Component<{ itemRating: number }> {
+  itemRating;
 
-  return (
-    <div className='rating-wrapper'>
-      {starsArray.map((imgSrc) => (
-        <img key={uuidv4()} src={imgSrc} alt='Raiting' />
-      ))}
-    </div>
-  );
-};
+  starsArray;
+
+  constructor(props: { itemRating: number }) {
+    super(props);
+    this.itemRating = props.itemRating;
+    this.starsArray = ratingVisualizationFn(this.itemRating);
+  }
+
+  render() {
+    return (
+      <div className='rating-wrapper'>
+        {this.starsArray.map((imgSrc) => (
+          <img key={uuidv4()} src={imgSrc} alt='Raiting' />
+        ))}
+      </div>
+    );
+  }
+}
+
 export default ItemRating;
